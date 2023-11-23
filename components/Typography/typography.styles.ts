4ba90a4d45d4957d2d03fc7521ea07d4874134bb
rebeccahongsf <rebeccahongsf@gmail.com>
2023-11-22 17:06:57 -0800

    add Typography components

---
+++ components/Typography/typography.styles.ts
@@ -0,0 +1,56 @@
+export const fontFamilies = {
+  sans: 'font-sans',
+  serif: 'font-serif',
+};
+
+export const fontWeights = {
+  normal: 'font-normal',
+  semibold: 'font-semibold',
+  bold: 'font-bold',
+};
+
+// Modular typography - already includes responsive font sizes
+export const fontSizes = {
+  base: 'type-0',
+  1: 'type-1',
+  2: 'type-2',
+  3: 'type-3',
+  4: 'type-4',
+  5: 'type-5',
+  6: 'type-6',
+  7: 'type-7',
+  8: 'type-8',
+  9: 'type-9',
+  hero: 'hero',
+};
+
+// Line heights
+export const fontLeadings = {
+  none: 'leading-none', // 1
+  tight: 'leading-tight', // 1.1
+  display: 'leading-display', // 1.2
+  snug: 'leading-snug', // 1.3
+  cozy: 'leading-cozy', // 1.4
+  normal: 'leading', // 1.5
+  trim: 'leading-trim', // 0.75
+};
+
+export const textAligns = {
+  left: 'text-left',
+  center: 'text-center',
+  right: 'text-right',
+};
+
+export const textColors = {
+  default: '', // Interit from the base
+  black: 'text-black',
+  white: 'text-white',
+};
+
+export const textVariants = {
+  none: '',
+  big: 'big-paragraph',
+  subheading: 'subheading',
+};
+
+export const iconStyle = 'inline-block shrink-0 w-09em ml-03em -mt-01em';
