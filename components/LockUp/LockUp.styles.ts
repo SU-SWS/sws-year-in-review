163845176bc85b05e42b01e0ad3276a00aee4862
Yvonne Tang <ytang2@stanford.edu>
2024-01-05 16:03:14 -0800

    Add Stanford font and update font usage; update masthead

--- components/LockUp/LockUp.styles.ts
+++ components/LockUp/LockUp.styles.ts
@@ -1,6 +1,6 @@
 export const root = 'lockup no-underline inline-block';
 export const logo = 'text-[1.43em] leading-half mt-[0.27em]';
-export const bar = 'w-1 h-[1em] mx-03em';
+export const bar = 'w-1 h-1em mx-03em';
 export const text = 'text-[0.95em] sm:text-[1.1em] font-normal mt-[0.25em]';
 export const textColor = {
   default: 'text-black',
--- components/Masthead/Masthead.tsx
+++ components/Masthead/Masthead.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
 import { LockUp } from '../LockUp';
-import { FlexBox } from '../FlexBox';
-import { Link } from '../Link';
 import * as styles from './Masthead.styles';
 
 export const Masthead = () => {
 
   return (
-    <div>
-      <FlexBox
-        as="header"
-        alignItems="center"
-        justifyContent="between"
-        className={styles.root}
-      >
-        <Link href="/" className={styles.logoLink}>
-          <div className={styles.lockUpWrapper}>
-            <LockUp text="Web Services" />
-          </div>
-        </Link>
-      </FlexBox>
+    <div className="cc bg-black rs-py-4">
+      <LockUp text="Web Services" color="white" />
     </div>
   );
 };
