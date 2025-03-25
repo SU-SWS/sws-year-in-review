ec3b5217a0a36f83028891e5c86afd76947d89bf
rebeccahongsf <rebeccahongsf@gmail.com>
2025-03-25 16:14:09 -0700

    fixup

--- components/PageSections/GoTeam.tsx
+++ components/PageSections/GoTeam.tsx
@@ -6,12 +6,14 @@ import { ActionLink } from '../Cta';
 export default function GoTeam() {
   return (
     <section className="rs-mb-10">
-      <Heading>
-        Our team looks forward to working with yours in{' '}
-        <Text as='span' bgColor="illuminating" color="black-true" className="px-10 rounded-[0.8rem] text-black-true">
-          2024.
-        </Text>
-      </Heading>
+      <div className="max-w-prose">
+        <Heading>
+          Our team looks forward to working with yours in{' '}
+          <Text as='span' bgColor="illuminating" color="black-true" className="px-10 rounded-[0.8rem] text-black-true">
+            2024.
+          </Text>
+        </Heading>
+      </div>
       <Card paddingType='small' className='rs-mt-5 max-w-1000 mx-auto'>
         <Text size={2} className="text-black-30 rs-mb-0">
           Interest in having your own <span className="font-semibold">Year in Review</span> or <span className="font-semibold">Annual Report site</span> like
