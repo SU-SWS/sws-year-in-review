ce9ec9601e47077a610e28bd8b29377043147e73
rebeccahongsf <rebeccahongsf@gmail.com>
2025-03-25 16:13:06 -0700

    fixup

--- components/PageSections/IntroSection.tsx
+++ components/PageSections/IntroSection.tsx
@@ -4,7 +4,7 @@ import { ArrowDownIcon } from '@heroicons/react/16/solid';
 export const IntroSection = () => (
   <section className="rs-mt-8 rs-mb-10 h-full cc">
     <div className="relative">
-    <div className="max-w-[120rem]">
+    <div className="max-w-[110rem]">
       <Heading as="h1" size={8} leading="tight" className="rs-mb-2">
         2023 Year in review.
       </Heading>
@@ -32,12 +32,12 @@ export const IntroSection = () => (
         .
       </Paragraph>
     </div>
-    <div className="flex w-full justify-start md:justify-end">
+    {/* <div className="flex w-full justify-start md:justify-end">
       <a href="#main-content" className="group text-24 text-white no-underline hocus:underline hocus:text-digital-red-xlight">
         Scroll to learn more
         <ArrowDownIcon width={30} className="transition-all ease-in-out duration-1000 ml-03em group-hocus:translate-y-02em text-white group-hocus:text-digital-red-xlight inline-block" />
       </a>
-    </div>
+    </div> */}
     </div>
   </section>
 );
